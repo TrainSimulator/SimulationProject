e28604123ad395cd9925dc4095277ce3d979b3ae
alex_gehrke <alexander.gehrke@epfl.ch>
2019-03-26 19:01:03 +0100

    Additional performance indicators

--- main.m
+++ main.m
@@ -4,8 +4,20 @@ close all
 
 tic;
 
-scenario = NewScenario(1);
-[times, queues] = Simulation(scenario);
-plot(times, queues)
+rng default;
+
+scenario = NewScenario(2);
+[times, queues, waittime, gain, loss] = Simulation(scenario);
+
+profit = gain - loss;
+avgwaittime = mean(waittime);
+varwaittime = var(waittime);
+maxwaittime = max(waittime);
+minwaittime = min(waittime);
+waittime95perc = prctile(waittime,0.95);
+
+figure; plot(times/60, queues);
+figure; histogram(waittime);
+% figure; boxplot(waittime);
 
 toc
