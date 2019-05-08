154d831e7c898af1e9df0f3f2c6a0ee27ceea5f3
alex_gehrke <alexander.gehrke@epfl.ch>
2019-05-08 10:02:21 +0200

    more computation time improvements

--- SimulationF.m
+++ SimulationF.m
@@ -10,20 +10,26 @@ PEventList = PassengerEvents(scenario);
 TEventList = TrainEvents(scenario);
 EventList = [PEventList; TEventList];
 
-npassengers = length(PEventList);
+% npassengers = length(PEventList);
 nevents = length(EventList);
 ntrains = length(scenario.timetable);
+
+nstat = 4; % Total number of stations
+nclass = 2; % Total number of classes (1st, 2nd)
 
 % Sort events for time:
 [EventList, ~] = sortrows(EventList,1);
 
 % Preallocate queues for stations and trains
-queue(3).p = zeros(0);
-% trains(ntrains,2).p = zeros(0);
-trains = zeros(ntrains,2,3); % (trainID, class, destinations 1: Lausanne, 2: Bern, 3: Zuerich)
+queue(nstat-1, nclass).p = zeros(0);
+trains = zeros(ntrains,nclass,nstat-1); % (trainID, class, destinations 1: Lausanne, 2: Bern, 3: Zuerich)
+
+ndest = zeros(nclass,nstat-1);
+nqueues = zeros(nstat-1,nclass); % Queue counter variable
+ntrans = zeros(1,2);
 
 % Preallocate queue book keeping vectors
-queues = zeros(nevents,3);
+queues = zeros(nevents,nstat-1);
 times = zeros(nevents,1);
 
 % Indicators:
@@ -34,43 +40,50 @@ loss = 0;
 %% Handle event list:
 for j = 1:nevents
     % Get entries from Event:
-    Event = EventList(j,:);
-    t = Event(1);
-    type = Event(2);
-    station = Event(3);
+    t = EventList(j,1);
+    type = EventList(j,2);
+    station = EventList(j,3);
     
     switch type
         case 1 % Passenger generation
-            queue(station).p(end+1) = j;
+            class = EventList(j,6);
+            queue(station,class).p(end+1) = j;
+            nqueues(station,class) = nqueues(station,class) + 1;
         case 2 % Train arrival
-            trainID = Event(5);
+            trainID = EventList(j,5);
             %% 1. Empty train:
             if station ~= 1
                 trains(trainID,:,station-1) = 0;
             end
             %% 2. Board train:
-            if station ~= 4
-                nqueue = length(queue(station).p); % Number of passengers in station queue
+            if station ~= nstat % If train at final stop
                 npass = [sum(trains(trainID,1,:)) sum(trains(trainID,2,:))];
-                i = 1;
-                while i <= nqueue
-                    % If passenger fits, put him on the train and remove
-                    % from queue, add ticket price
-                    class = EventList(queue(station).p(i),6);
-                    if npass(class) < scenario.capacity(trainID,class)
-                        destination = EventList(queue(station).p(i),4);
-                        gain = gain + scenario.ticket(class) * (destination - station);
-                        waittime(end+1) = t - EventList(queue(station).p(i),1);
-                        trains(trainID,class,destination-1) = trains(trainID,class,destination-1) + 1;
-                        queue(station).p(i) = [];
-                        nqueue = nqueue - 1;
-                        npass(class) = npass(class) + 1;
+                space = scenario.capacity(trainID,:) - npass;
+                fullq = space > nqueues(station,:);
+                
+                addtimes = [];
+                for class = 1:nclass
+                    if nqueues(station,class) ~= 0
+                        if fullq(class)
+                            ndest(class,:) = sum(EventList(queue(station,class).p,4) == 2:nstat);
+                        else
+                            ndest(class,:) = sum(EventList(queue(station,class).p(1:space(class)),4) == 2:nstat);
+                        end
+                        ntrans(class) = sum(ndest(class,:));
+                        for dest = 1:nstat-1
+                            trains(trainID,class,dest) = trains(trainID,class,dest) + ndest(class,dest);
+                            gain = gain + ndest(class,dest) * (scenario.ticket(class) * (dest + 1 - station));
+                        end
+                        addtimes = [addtimes t - EventList(queue(station,class).p(1:ntrans(class)),1)'];
+                        queue(station,class).p(1:ntrans(class)) = [];
                     else
-                        % Passenger misses train
-                        loss = loss + scenario.missed(class);
-                        i = i + 1;
+                        ntrans(class) = 0;
                     end
                 end
+                waittime = [waittime addtimes];
+                npass = npass + ntrans;
+                nqueues(station,:) = nqueues(station,:) - ntrans;
+                loss = loss + sum(nqueues(station,:) .* scenario.missed);
             end
             %% 3. After boarding:
             % Cost for empty seats on train
@@ -79,6 +92,6 @@ for j = 1:nevents
     end
     %% Book keeping for each event:
     times(j) = t;
-    queues(j,:) = [length(queue(1).p) length(queue(2).p) length(queue(3).p)];
+    queues(j,:) = nqueues(:,1) + nqueues(:,2);
 end
 end
