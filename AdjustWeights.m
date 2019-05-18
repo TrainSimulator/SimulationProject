f22405d4de597c039642babb16bcdd749bd34f75
TrainSimulator <alexander.gehrke@epfl.ch>
2019-05-18 14:07:49 +0200

    Only increase weights for higher inequality between weights

--- AdjustWeights.m
+++ AdjustWeights.m
@@ -1,19 +1,22 @@
 function weights = AdjustWeights(weights, idx, pos, level)
 %% Adjust neighbourhood weights
-totalneighbours = length(weights);
-nneighbourhoods = 30;
-% Change sign based on improvement or not 
 if pos
-    sign = 1;
-else
-    sign = -1;
+    weights(idx) = weights(idx) + 1;
 end
-
-if level == 2
-    weights = weights - sign * 1;
-    weights(idx) = weights(idx) + sign * (totalneighbours + 1);
-else
-    weights(1:nneighbourhoods) = weights(1:nneighbourhoods) - sign * 1;
-    weights(idx) = weights(idx) + sign * (nneighbourhoods + 1);
-end
+% totalneighbours = length(weights);
+% nneighbourhoods = 30;
+% % Change sign based on improvement or not 
+% if pos
+%     sign = 1;
+% else
+%     sign = -1;
+% end
+% 
+% if level == 2
+%     weights = weights - sign * 1;
+%     weights(idx) = weights(idx) + sign * (totalneighbours + 1);
+% else
+%     weights(1:nneighbourhoods) = weights(1:nneighbourhoods) - sign * 1;
+%     weights(idx) = weights(idx) + sign * (nneighbourhoods + 1);
+% end
 end
